960befaa7dbad4ecdfd7cbd1cb860c500c4ab009
CrisKoch <cristinak.iftm@gmail.com>
2020-05-13 19:49:08 -0300

    Leitura da escolaridade

--- src/candidato.java
+++ src/candidato.java
@@ -1,9 +1,29 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class candidato {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		int esc;
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.print("Digite uma opção: ");
+		
+		esc = sc.nextInt();
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
